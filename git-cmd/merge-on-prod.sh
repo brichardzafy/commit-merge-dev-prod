ab20aa8b0822065b38f614cda559fd386765c4a7
brichard <brichard.zafy@gmail.com>
2022-04-06 19:27:48 +0200

    Maj text

--- git-cmd/merge-on-prod.sh
+++ git-cmd/merge-on-prod.sh
@@ -6,7 +6,7 @@ echo "Répertoire Actuel : $current_rep " ;
 current_branch=`git rev-parse --abbrev-ref HEAD`
 #@todo : A remplacer selon nom de la branche qui fait le deploiement en PROD
 branch_prod="prod-v3"
-echo "Branch Actuel : $current_rep " ;
+echo "Branch Actuel : $branch_prod " ;
 git checkout $branch_prod
 git pull origin $branch_prod
 git merge $current_branch
