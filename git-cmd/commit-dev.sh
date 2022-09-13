8edc778436228889edfb86cf2ee6901b2d53644a
Brichard.Z <brichard.zafy@gmail.com>
2022-09-13 10:22:04 +0300

    Merge master to dev

--- git-cmd/commit-dev.sh
+++ git-cmd/commit-dev.sh
@@ -14,7 +14,7 @@ else
 fi
 current_branch=`git rev-parse --abbrev-ref HEAD`
 #@todo : A remplacer selon nom de la branche qui fait le deploiement en DEV
-branch_dev="dev-v3"
+branch_dev="dev"
 git checkout $branch_dev
 git pull origin $branch_dev
 git merge $current_branch
